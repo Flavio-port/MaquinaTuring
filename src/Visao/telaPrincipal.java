2804a7cf9f00218898b7d3ace62b2aef2fbafe80
junior <junior@DESKTOP-2QUI20D>
2019-06-18 21:53:12 -0300

    Ajuste Final

--- src/Visao/telaPrincipal.java
+++ src/Visao/telaPrincipal.java
@@ -45,6 +45,7 @@ public class telaPrincipal extends javax.swing.JFrame {
         tabelaAcoes = new javax.swing.JTable();
         jLabel4 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
+        jLabel5 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("telaPrincipal");
@@ -59,7 +60,7 @@ public class telaPrincipal extends javax.swing.JFrame {
             }
         });
         getContentPane().add(txtFita);
-        txtFita.setBounds(234, 27, 262, 29);
+        txtFita.setBounds(130, 20, 262, 29);
 
         btnCriaTabela.setText("Cria Tabela de Ações");
         btnCriaTabela.addActionListener(new java.awt.event.ActionListener() {
@@ -70,9 +71,15 @@ public class telaPrincipal extends javax.swing.JFrame {
         getContentPane().add(btnCriaTabela);
         btnCriaTabela.setBounds(396, 74, 133, 23);
         getContentPane().add(txtQuantidadeEstado);
-        txtQuantidadeEstado.setBounds(234, 75, 98, 20);
+        txtQuantidadeEstado.setBounds(220, 80, 90, 30);
+
+        txtTokens.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtTokensActionPerformed(evt);
+            }
+        });
         getContentPane().add(txtTokens);
-        txtTokens.setBounds(234, 108, 98, 20);
+        txtTokens.setBounds(220, 120, 90, 30);
 
         jLabel1.setText("Informe a Fita :");
         getContentPane().add(jLabel1);
@@ -80,11 +87,11 @@ public class telaPrincipal extends javax.swing.JFrame {
 
         jLabel2.setText("Informe a quantidade de Estados :");
         getContentPane().add(jLabel2);
-        jLabel2.setBounds(18, 78, 210, 14);
+        jLabel2.setBounds(20, 90, 210, 14);
 
         jLabel3.setText("Informe os Tokens :");
         getContentPane().add(jLabel3);
-        jLabel3.setBounds(18, 114, 180, 14);
+        jLabel3.setBounds(90, 130, 180, 14);
 
         tabelaAcoes.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -100,12 +107,12 @@ public class telaPrincipal extends javax.swing.JFrame {
         jScrollPane1.setViewportView(tabelaAcoes);
 
         getContentPane().add(jScrollPane1);
-        jScrollPane1.setBounds(20, 178, 510, 150);
+        jScrollPane1.setBounds(20, 200, 510, 220);
 
-        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel4.setText("Os tokens de espaço e simbolo inicial já estão cadastrados");
+        jLabel4.setBackground(new java.awt.Color(0, 0, 0));
+        jLabel4.setText("Os tokens de espaço (_) e simbolo inicial (>) já estão cadastrados");
         getContentPane().add(jLabel4);
-        jLabel4.setBounds(20, 139, 280, 14);
+        jLabel4.setBounds(20, 160, 510, 14);
 
         jButton1.setText("Executa Maquina");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -116,7 +123,11 @@ public class telaPrincipal extends javax.swing.JFrame {
         getContentPane().add(jButton1);
         jButton1.setBounds(400, 120, 130, 23);
 
-        setSize(new java.awt.Dimension(632, 428));
+        jLabel5.setText("Utilize o simbolo > como inicial");
+        getContentPane().add(jLabel5);
+        jLabel5.setBounds(180, 50, 150, 14);
+
+        setSize(new java.awt.Dimension(632, 481));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
@@ -124,8 +135,7 @@ public class telaPrincipal extends javax.swing.JFrame {
              fita= txtFita.getText().trim();
              String numeroEstado= txtQuantidadeEstado.getText().trim();
              String Tokens= txtTokens.getText().trim();
-             modelo = new ModeloTabela(5,"a,b");
-   
+             modelo = new ModeloTabela(Integer.parseInt(numeroEstado),Tokens.toUpperCase());                          
              tabelaAcoes.setModel(modelo);
              tabelaAcoes.setVisible(rootPaneCheckingEnabled);
     }//GEN-LAST:event_btnCriaTabelaActionPerformed
@@ -137,8 +147,24 @@ public class telaPrincipal extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         Maquina maquina = new Maquina();
-        maquina.analisaFita(fita, modelo);
+        modelo.setValueAt1(0, 1, "0,>,d");
+         modelo.setValueAt1(0, 2, "0,*,D");
+          modelo.setValueAt1(0, 3, "1,_,D");
+           modelo.setValueAt1(1, 2, "1,*,d");
+            modelo.setValueAt1(1, 3, "Fim,_,d");
+       try{
+            String resultado =maquina.analisaFita(">*** **", modelo);//fita
+            JOptionPane.showMessageDialog(this, "Executado com sucesso o seu resultado é: "+ resultado);
+       }catch(Exception ex){
+           JOptionPane.showMessageDialog(this, "Erro em sua fita ou em sua tabela de ações");
+       }
+       
+        
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void txtTokensActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtTokensActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtTokensActionPerformed
 
     /**
      * @param args the command line arguments
@@ -182,6 +208,7 @@ public class telaPrincipal extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tabelaAcoes;
     private javax.swing.JTextField txtFita;
--- src/tabela/ModeloTabela.java
+++ src/tabela/ModeloTabela.java
@@ -15,9 +15,10 @@ import javax.swing.table.TableModel;
 public class ModeloTabela extends AbstractTableModel {
 
     private Map<Integer,List<String>> linhas = new HashMap<>();
-    private List<String> colunas = Stream.of(new String[]{"Estado", ">", "*"}).collect(Collectors.toList());
+    private List<String> colunas = Stream.of(new String[]{"Estado", ">", "*", "_"}).collect(Collectors.toList());
 
     public ModeloTabela(int qtdEstados, String tokens) {
+        
         
         if(tokens != null || tokens.trim().length() > 0){
             
@@ -35,7 +36,7 @@ public class ModeloTabela extends AbstractTableModel {
         for(int i = 0; i < qtdEstados ; i++) {
             
             ArrayList<String> col = new ArrayList();
-            col.add(i+1 + "");
+            col.add(i + "");
             
             for(int j = 1; j < colunas.size(); j++) {
                 col.add("");
@@ -44,53 +45,6 @@ public class ModeloTabela extends AbstractTableModel {
             linhas.put(i, col);
         }
     }
-
-//    public void montaTabela() {
-//
-//        int contColunas = 0;
-//        Tokens coluna = new Tokens();
-//        coluna.setCod(contColunas);
-//        coluna.setNome("Estado"); 
-//        colunas.add(coluna);
-//        contColunas++;
-//        
-//        
-//        
-//       
-//        coluna = new Tokens();
-//        coluna.setCod(contColunas);
-//        coluna.setNome(">"); 
-//        colunas.add(coluna);
-//        contColunas++;
-//        
-//        coluna = new Tokens();
-//        coluna.setCod(contColunas);
-//        coluna.setNome("*"); 
-//        colunas.add(coluna);
-//        contColunas++;
-//        
-//        
-//
-//        String aux = "";
-//  
-//        for (int i = 0; i < Integer.parseInt(estados.trim()); i++) {
-//            Estado est = new Estado(i);
-//            linhas.add(est);
-//        }
-//        for (int i = 0; i < tokens.length(); i++) {
-//
-//            if (tokens.charAt(i) == ',' || tokens.charAt(i) == ' ' || i == tokens.length()) {
-//                coluna = new Tokens();
-//                coluna.setCod(contColunas);
-//                coluna.setNome(aux.trim());
-//                colunas.add(coluna);
-//                aux = "";
-//            } else {
-//                aux += tokens.charAt(i);
-//            }
-//        }
-//     
-//    }
 
     @Override
     public int getRowCount() {
@@ -131,14 +85,14 @@ public class ModeloTabela extends AbstractTableModel {
     }
     
     
-    public String getTransicao(Integer estado, String token){
+    public String getTransicao(Integer estado, char token){
         List<String> linha = linhas.get(estado);
-        
+        String tokens=""+token;
         if(linha != null){
             int idxColuna = 0;
             
             for(; idxColuna < colunas.size(); idxColuna ++){
-                if(token.equals(colunas.get(idxColuna))){
+                if(tokens.equals(colunas.get(idxColuna))){
                     break;
                 }
             }
@@ -150,78 +104,10 @@ public class ModeloTabela extends AbstractTableModel {
         
         return null;
     }
-     
-    
+
+    public void setValueAt1(int i, int i0, String d) {
+       linhas.get(i).set(i0, d);
+       fireTableDataChanged();
+    }
+      
 }
-
-/* //retorna se a célula é editável ou não
-    @Override
-    public boolean isCellEditable(int rowIndex, int columnIndex) {
-        return true;
-    }
-
-    //retorna o total de itens(que virarão linhas) da nossa lista
-    @Override
-    public int getRowCount() {
-        return estados.size();
-    }
-    //retorna o total de colunas da tabela
-    @Override
-    public int getColumnCount() {
-        return colunas.length;
-    }
-    //retorna o nome da coluna de acordo com seu indice
-    @Override
-    public String getColumnName(int indice) {
-        return colunas[indice];
-    }
-
-    //determina o tipo de dado da coluna conforme seu indice
-    @Override
-    public Class<?> getColumnClass(int columnIndex) {
-             return String.class;      
-    }
-
-    //preenche cada célula da tabela
-    @Override
-    public Object getValueAt(int rowIndex, int columnIndex) {
-        Funcionario funcionario = this.funcionarios.get(rowIndex);
-
-        switch (columnIndex) {
-            case COLUNA_NOME:
-                return funcionario.getNome();
-            case COLUNA_IDADE:
-                return funcionario.getIdade();
-            case COLUNA_MATRICULA:
-                return funcionario.getMatricula();
-            case COLUNA_ADMITIDO:
-                return funcionario.isAdmitido();
-        }
-        return null;
-    }
-    //altera o valor do objeto de acordo com a célula editada
-    //e notifica a alteração da tabela, para que ela seja atualizada na tela
-    @Override
-    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-        //o argumento recebido pelo método é do tipo Object
-        //mas como nossa tabela é de funcionários, é seguro(e até recomendável) fazer o cast de suas propriedades
-        Funcionario funcionario = this.funcionarios.get(rowIndex);
-        //de acordo com a coluna, ele preenche a célula com o valor
-        //respectivo do objeto de mesmo indice na lista
-        switch (columnIndex) {
-            case COLUNA_NOME:
-                funcionario.setNome(String.valueOf(aValue));
-                break;
-            case COLUNA_IDADE:
-                funcionario.setIdade((int) aValue);
-                break;
-            case COLUNA_MATRICULA:
-                funcionario.setMatricula((int) aValue);
-                break;
-            case COLUNA_ADMITIDO:
-                funcionario.setAdmitido((boolean) aValue);
-        }
-        //este método é que notifica a tabela que houve alteração de dados
-        fireTableDataChanged();
-    }
-}*/
